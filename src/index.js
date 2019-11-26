e503448a91e7a4b7c5041537523abc08e451c65c
architekespy <architekespy@Reginalds-MacBook-Pro.local>
2019-11-26 14:40:49 -0500

    Lets get started on FeaturedRooms

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
-
-
 import {BrowserRouter as Router} from "react-router-dom";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import {RoomProvider} from './context';
 
 ReactDOM.render(
-    <Router>
+    <RoomProvider>
+
+        <Router>
         <App />
-    </Router>, 
+        </Router>
+
+    </RoomProvider>
+    , 
     document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
