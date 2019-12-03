40dd209c70c653d55a93376bb5a803f35a2ede4f
architekespy <architekespy@Reginalds-MacBook-Pro.local>
2019-12-03 13:33:35 -0500

    progress

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import {RoomContext} from "../context";
 
 export default class FeaturedRooms extends Component {
+    static contextType = RoomContext
     render() {
+        
         return (
             <div>
-                Hello from FeaturedRooms Rooms!
+             from FeaturedRooms
             </div>
-        )
+        );
     }
 }
